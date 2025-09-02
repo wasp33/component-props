56d7a90d77c03d57c96591d14de6f26b4b47f25f
Joseph Liu <drowsy.liu@gmail.com>
2025-09-02 10:44:27 +0800

    base url fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/compoment-props/',
+  base: '/component-props/',
   plugins: [vue(), tailwindcss(),],
 })
